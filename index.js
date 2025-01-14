d52e9316b9f48a8578dab1b18cda4866375a4b9a
NNmaki <92058983+NNmaki@users.noreply.github.com>
2025-01-14 23:11:31 +0200

    Commit

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com", "www.nnmaki.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-// let inputData = document.getElementById("input-el").value;
+let inputData = document.getElementById("input-el").value;
 
 
 
 inputBtn.addEventListener("click", function() {
-    // let inputData = document.getElementById("input-el").value;
+    let inputData = document.getElementById("input-el").value;
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
