8845cd49d9977a00585a14b947190c9807804fe5
NNmaki <nikon.niittymaki@gmail.com>
2025-01-21 16:20:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com", "www.nnmaki.com"]
+// let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com", "www.nnmaki.com"]
+
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 let inputData = document.getElementById("input-el").value;
 
-// testing
-
 inputBtn.addEventListener("click", function() {
     let inputData = document.getElementById("input-el").value;
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
+    inputEl.value = "";
+
+    // document.getElementById("input-el").value = "";
 })
 
-// 1. Create a variable, listItems, to hold all the HTML for the list items
-// Assign it to an empty string to begin with
 
-let listItems = ""
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // listItems += "<li>" + "<a href='"+myLeads[i]+"'></a>" +myLeads[i]+ "</li>"
+        listItems += "<li><a target='_blank' href= '" + myLeads[1] +  "'>" + myLeads[i] + "</a></li>"
 
-for (let i = 0; i < myLeads.length; i++) {
-    // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
-    listItems += "<li>" + myLeads[i] + "</li>"
+
+
+    }
+    ulEl.innerHTML = listItems
 }
 
-ulEl.innerHTML = listItems
-
-// 3. Render the listItems inside the unordered list using ulEl.innerHTML
