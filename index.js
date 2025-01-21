17cb1f835c9ebbf95cb659d7cf9d30b38d44f1ce
NNmaki <nikon.niittymaki@gmail.com>
2025-01-21 16:45:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 
-// let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com", "www.nnmaki.com"]
+
 
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-let inputData = document.getElementById("input-el").value;
-
 inputBtn.addEventListener("click", function() {
     let inputData = document.getElementById("input-el").value;
     myLeads.push(inputEl.value)
     renderLeads()
     inputEl.value = "";
-
-    // document.getElementById("input-el").value = "";
 })
 
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        // listItems += "<li>" + "<a href='"+myLeads[i]+"'></a>" +myLeads[i]+ "</li>"
-        listItems += "<li><a target='_blank' href= '" + myLeads[1] +  "'>" + myLeads[i] + "</a></li>"
-
-
-
+        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        
     }
     ulEl.innerHTML = listItems
 }
